914202a1222cde1e2fde3d20dd07810e08aabf27
aliya mahaboob <mahaboobaliya02@gmail.com>
2025-10-30 10:11:14 +0530

    updated logicto get animated cats gif only

--- index.js
+++ index.js
@@ -3,26 +3,48 @@ import { catsData } from "./data.js"
 
 const emotionRadios = document.getElementById("emotion-radios")
 const getImageBtn = document.getElementById("get-image-btn");
-
+const gifOnlyOption = document.getElementById("gifs-only-option");
 
 
 emotionRadios.addEventListener("change", highlightCheckedOption)
-getImageBtn.addEventListener("click", getMatchingCatsArray)
-    
-function highlightCheckedOption(e){
+getImageBtn.addEventListener("click", renderCat)
+
+function highlightCheckedOption(e) {
     const radios = document.getElementsByClassName('radio')
-    for (let radio of  radios) {
+    for (let radio of radios) {
         radio.classList.remove('highlight')
     }
     // remove all instances of the highlight class
     document.getElementById(e.target.id).parentElement.classList.add('highlight')
 }
 
-function getMatchingCatsArray(){
-   if(document.querySelector('input[type="radio"]:checked')) {
-       const selectedEmotionValue = document.querySelector('input[type="radio"]:checked').value
-       console.log(selectedEmotionValue)
-   }
+function getMatchingCatsArray() {
+
+    if (document.querySelector('input[type="radio"]:checked')) {
+        const selectedEmotion = document.querySelector('input[type="radio"]:checked').value
+        const isGif = gifOnlyOption.checked;
+        const matchingCatsArray = catsData.filter((cats) => {
+            if (isGif) {
+                return cats.isGif && cats.emotionTags.includes(selectedEmotion)
+            } else {
+                return cats.emotionTags.includes(selectedEmotion)
+            }
+        })
+        return matchingCatsArray
+    }
+}
+
+function getSingleCatObject(){
+    const catsArray = getMatchingCatsArray()
+    if (catsArray.length === 1) {
+        console.log(catsArray[0])
+    }
+    
+}
+
+function renderCat(){
+    getSingleCatObject()
+    
 }
 
 function getEmotionsArray(cats) {
@@ -30,7 +52,7 @@ function getEmotionsArray(cats) {
     for (let cat of cats) {
         for (let emotion of cat.emotionTags) {
             if (!emotionsArray.includes(emotion)) {
-                    emotionsArray.push(emotion)
+                emotionsArray.push(emotion)
             }
         }
     }

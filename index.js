6ce59c13a05ae75e2444ecdf1fc7cba23b84790c
aliya mahaboob <mahaboobaliya02@gmail.com>
2025-10-30 06:10:13 +0530

    queryselector

--- index.js
+++ index.js
@@ -2,16 +2,27 @@ import { catsData } from "./data.js"
 
 
 const emotionRadios = document.getElementById("emotion-radios")
+const getImageBtn = document.getElementById("get-image-btn");
+
 
 
 emotionRadios.addEventListener("change", highlightCheckedOption)
+getImageBtn.addEventListener("click", getMatchingCatsArray)
     
 function highlightCheckedOption(e){
     const radios = document.getElementsByClassName('radio')
     for (let radio of  radios) {
         radio.classList.remove('highlight')
     }
+    // remove all instances of the highlight class
     document.getElementById(e.target.id).parentElement.classList.add('highlight')
+}
+
+function getMatchingCatsArray(){
+   if(document.querySelector('input[type="radio"]:checked')) {
+       const selectedEmotionValue = document.querySelector('input[type="radio"]:checked').value
+       console.log(selectedEmotionValue)
+   }
 }
 
 function getEmotionsArray(cats) {
